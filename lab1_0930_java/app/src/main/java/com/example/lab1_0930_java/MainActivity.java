f9984114564c4d4229b3da0d23853c3002569386
yanyu0706 <r0920075266@gmail.com>
2022-09-30 11:01:03 +0800

    lab2

---
+++ lab1_0930_java/app/src/main/java/com/example/lab1_0930_java/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lab1_0930_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
